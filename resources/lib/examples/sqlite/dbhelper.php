57d1b3b4db33c95cb2261d9b639fc7f23f72b806
SK-Ouchi <sch.skouchi@gmail.com>
2017-04-25 16:12:02 -0700

    Added methods for db

--- resources/lib/examples/sqlite/dbhelper.php
+++ resources/lib/examples/sqlite/dbhelper.php
@@ -54,6 +54,9 @@ function createTable(){
         echo 'Exception : '.$e->getMessage();
     }
 }
+
+//		***FACULTY***
+
 function insertFaculty($depart_id,$name,$email){
     global $conn;
     try {
@@ -66,6 +69,81 @@ function insertFaculty($depart_id,$name,$email){
         echo 'Exception : '.$e->getMessage();
     }
 }
+
+//Update
+
+function updateFaculty($faculty_id,$depart_id,$name,$email,$new_depart_id,$new_name,$new_email){
+    global $conn;
+    try {
+		//Update single record
+        if ($new_depart_id == '' && $new_name == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET email = '$new_email'
+			WHERE faculty_id = '$faculty_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_email == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET name = '$new_name'
+			WHERE faculty_id = '$faculty_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_name == '' && $new_email == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET department_id = '$new_depart_id'
+			WHERE faculty_id = '$faculty_id;";
+			$conn->exec($sql);
+		}
+		//Mutiple record updates
+		else if ($new_depart_id == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET name = '$new_name', email = $new_email
+			WHERE faculty_id = '$faculty_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_name == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET department_id = '$new_depart_id', email = $new_email
+			WHERE faculty_id = '$faculty_id';";
+			$conn->exec($sql);
+		}
+		else if ($new_email == ''){
+			$sql = "UPDATE OR IGNORE Faculty
+			SET department_id = '$new_depart_id', name = '$new_name'
+			WHERE faculty_id = '$faculty_id';";
+			$conn->exec($sql);
+		}
+		//All record update
+		else{
+			$sql = "UPDATE OR IGNORE Faculty
+			SET department_id = '$new_depart_id', name = '$new_name', email = '$new_email'
+			WHERE faculty_id = '$faculty_id'";
+			$conn->exec($sql);
+		}
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Delete needs to be careful of children
+function deleteFaculty($faculty_id){
+    global $conn;
+    try {
+        // insert data
+        $sql = "DELETE OR IGNORE FROM Faculty
+				WHERE faculty_id = '$faculty_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//		***FACULTY END***
+
+//		***DEPARTMENT***
+
 function insertDepartment($name,$description){
     global $conn;
     try {
@@ -78,6 +156,493 @@ function insertDepartment($name,$description){
         echo 'Exception : ' . $e->getMessage();
     }
 }
+
+//separated the two updates into two functions for now. We can combine them later.
+
+function updateDepartName($name,$new_name){
+    global $conn;
+    try {
+        // update data name
+        $sql = "UPDATE OR IGNORE Departments 
+				SET depart_name = '$new_name'
+				WHERE depart_name = '$name';";
+        $conn->exec($sql);
+        echo "Updated data successfully <br>";
+    } catch (PDOException $e) {
+        echo 'Exception : ' . $e->getMessage();
+    }
+}
+
+function updateDepartDescription($new_description,$description){
+    global $conn;
+    try {
+        // update data description
+        $sql = "UPDATE OR IGNORE Departments 
+				SET description = '$new_description'
+				WHERE description = '$description';";
+        $conn->exec($sql);
+        echo "Updated data successfully <br>";
+    } catch (PDOException $e) {
+        echo 'Exception : ' . $e->getMessage();
+    }
+}
+
+function deleteDepartment($depart_id){
+    global $conn;
+    try {
+        // update data description
+        $sql = "DELETE OR IGNORE FROM Departments 
+				WHERE department_id = '$depart_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    } catch (PDOException $e) {
+        echo 'Exception : ' . $e->getMessage();
+    }
+}
+
+//		***DEPARTMENT END***
+
+//		***COURSES***
+
+function insertCourses($depart_id,$course_name,$course_descrip){
+    global $conn;
+    try {
+        // insert data
+        $sql = "INSERT OR IGNORE INTO Courses (department_id, course_name, description) 
+                      VALUES ('$depart_id','$course_name','$course_descrip');";
+        $conn->exec($sql);
+        echo "Inserted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Update
+
+function updateCourses($course_id, $new_depart_id, $new_course_name, $new_course_descrip){
+    global $conn;
+    try {
+		//Update single record
+        if ($new_depart_id == '' && $new_course_name == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET description = '$new_course_descrip'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_course_descrip == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET course_name = '$new_course_name'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_name == '' && $new_course_descrip == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET department_id = '$new_depart_id'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		//Mutiple record updates
+		else if ($new_depart_id == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET course_name = '$new_course_name', description = $new_course_descrip
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_name == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET department_id = '$new_depart_id', description = $new_course_descrip
+			WHERE course_id = '$course_id';";
+			$conn->exec($sql);
+		}
+		else if ($new_course_descrip == ''){
+			$sql = "UPDATE OR IGNORE Courses
+			SET department_id = '$new_depart_id', course_name = '$new_course_name'
+			WHERE course_id = '$course_id';";
+			$conn->exec($sql);
+		}
+		//All record update
+		else{
+			$sql = "UPDATE OR IGNORE Courses
+			SET department_id = '$new_depart_id', course_name = '$new_course_name', description = '$new_course_descrip'
+			WHERE course_id = '$course_id'";
+			$conn->exec($sql);
+		}
+        echo "Updated data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Delete needs to be careful of children
+function deleteCourses($course_id){
+    global $conn;
+    try {
+        // insert data
+        $sql = "DELETE OR IGNORE FROM Cources
+				WHERE course_id = '$course_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//		***COURSES END***
+
+//		***QUESTIONS***
+
+function insertQuestions($course_id, $department_id, $question_name, $question_descrip){
+    global $conn;
+    try {
+        // insert data
+        $sql = "INSERT OR IGNORE INTO Questions (course_id, department_id, question_name, description) 
+                      VALUES ('$course_id, $department_id','$question_name','$question_descrip');";
+        $conn->exec($sql);
+        echo "Inserted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+function updateQuestions($question_id, $new_course_id, $new_depart_id, $new_question_name, $new_question_descrip){
+    global $conn;
+    try {
+		//One record update
+		if ($new_course_id == '' && $new_depart_id == '' && $new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET description = $new_question_descrip
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_depart_id == '' && $new_question_descrip == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET question_name = $new_question_name
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_question_descrip == '' && $new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET department_id = $new_depart_id
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_question_descrip == '' && $new_depart_id == '' && $new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET course_id = $new_course_id
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		//Update two records
+        else if ($new_depart_id == '' && $new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET description = '$new_question_descrip', course_id = $new_course_id
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_course_id == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET description = '$new_question_descrip', question_name = $new_question_name
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_question_descrip == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET question_name = '$new_question_name', course_id = $new_course_id 
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_question_descrip == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET question_name = '$new_question_name', department_id = $new_depart_id 
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET description = '$new_question_descrip', department_id = $new_depart_id 
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_question_name == '' && $new_question_descrip == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET department_id = '$new_depart_id', course_id = '$new_course_id'
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		//Three record updates
+		else if ($new_depart_id == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET question_name = '$new_question_name', description = '$new_question_descrip', course_id = '$new_course_id'
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET question_name = '$new_question_name', description = '$new_question_descrip', department_id = '$new_depart_id'
+			WHERE question_id = '$question_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_question_name == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET department_id = '$new_depart_id', description = '$new_question_descrip', course_id = '$new_course_id'
+			WHERE question_id = '$question_id';";
+			$conn->exec($sql);
+		}
+		else if ($new_question_descrip == ''){
+			$sql = "UPDATE OR IGNORE Questions
+			SET department_id = '$new_depart_id', question_name = '$new_question_name', course_id = '$new_course_id'
+			WHERE question_id = '$question_id';";
+			$conn->exec($sql);
+		}
+		//All record update
+		else{
+			$sql = "UPDATE OR IGNORE Questions
+			SET department_id = '$new_depart_id', question_name = '$new_question_name', description = '$new_question_descrip', course_id = $new_course_id
+			WHERE question_id = '$question_id'";
+			$conn->exec($sql);
+		}
+        echo "Updated data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Delete needs to be careful of children
+function deleteQuestions($question_id){
+    global $conn;
+    try {
+        // insert data
+        $sql = "DELETE OR IGNORE FROM Questions
+				WHERE question_id = '$question_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//		***QUESTIONS END***
+
+//		***ANSWERS***
+function insertAnswers($course_id, $department_id, $answer_name, $answer_descrip){
+    global $conn;
+    try {
+        // insert data
+        $sql = "INSERT OR IGNORE INTO Answers (course_id, department_id, answer_name, description) 
+                      VALUES ('$course_id, $department_id','$answer_name','$answer_descrip');";
+        $conn->exec($sql);
+        echo "Inserted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Update
+function updatAnswers($answer_id, $new_course_id, $new_depart_id, $new_answer_name, $new_answer_descrip){
+    global $conn;
+    try {
+		//One record update
+		if ($new_course_id == '' && $new_depart_id == '' && $new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET description = $new_answer_descrip
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_depart_id == '' && $new_answer_descrip == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET answer_name = $new_answer_name
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_answer_descrip == '' && $new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET department_id = $new_depart_id
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_answer_descrip == '' && $new_depart_id == '' && $new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET course_id = $new_course_id
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		//Update two records
+        else if ($new_depart_id == '' && $new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET description = '$new_answer_descrip', course_id = $new_course_id
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_course_id == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET description = '$new_answer_descrip', answer_name = $new_answer_name
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_answer_descrip == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET answer_name = '$new_answer_name', course_id = $new_course_id 
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_answer_descrip == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET answer_name = '$new_answer_name', department_id = $new_depart_id 
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == '' && $new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET description = '$new_answer_descrip', department_id = $new_depart_id 
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_answer_name == '' && $new_answer_descrip == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET department_id = '$new_depart_id', course_id = '$new_course_id'
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		//Three record updates
+		else if ($new_depart_id == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET answer_name = '$new_answer_name', description = '$new_answer_descrip', course_id = '$new_course_id'
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_id == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET answer_name = '$new_answer_name', description = '$new_answer_descrip', department_id = '$new_depart_id'
+			WHERE answer_id = '$answer_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_answer_name == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET department_id = '$new_depart_id', description = '$new_answer_descrip', course_id = '$new_course_id'
+			WHERE answer_id = '$answer_id';";
+			$conn->exec($sql);
+		}
+		else if ($new_answer_descrip == ''){
+			$sql = "UPDATE OR IGNORE Answers
+			SET department_id = '$new_depart_id', answer_name = '$new_answer_name', course_id = '$new_course_id'
+			WHERE answer_id = '$answer_id';";
+			$conn->exec($sql);
+		}
+		//All record update
+		else{
+			$sql = "UPDATE OR IGNORE Answers
+			SET department_id = '$new_depart_id', answer_name = '$new_answer_name', description = '$new_answer_descrip', course_id = $new_course_id
+			WHERE answer_id = '$answer_id'";
+			$conn->exec($sql);
+		}
+        echo "Updated data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Delete needs to be careful of children
+function deleteAnswers($answer_id){
+    global $conn;
+    try {
+        // insert data
+        $sql = "DELETE OR IGNORE FROM Answers
+				WHERE answer_id = '$answer_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//		***ANSWERS END***
+
+//		***RESULTS***
+function insertResults($depart_id,$course_name,$result_descrip){
+    global $conn;
+    try {
+        // insert data
+        $sql = "INSERT OR IGNORE INTO Results (department_id, course_name, description) 
+                      VALUES ('$depart_id','$course_name','$result_descrip');";
+        $conn->exec($sql);
+        echo "Inserted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Update
+
+function updateResults($course_id, $new_depart_id, $new_course_name, $new_result_descrip){
+    global $conn;
+    try {
+		//Update single record
+        if ($new_depart_id == '' && $new_course_name == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET description = '$new_result_descrip'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_depart_id == '' && $new_result_descrip == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET course_name = '$new_course_name'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_name == '' && $new_result_descrip == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET department_id = '$new_depart_id'
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		//Mutiple record updates
+		else if ($new_depart_id == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET course_name = '$new_course_name', description = $new_result_descrip
+			WHERE course_id = '$course_id;";
+			$conn->exec($sql);
+		}
+		else if ($new_course_name == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET department_id = '$new_depart_id', description = $new_result_descrip
+			WHERE course_id = '$course_id';";
+			$conn->exec($sql);
+		}
+		else if ($new_result_descrip == ''){
+			$sql = "UPDATE OR IGNORE Results
+			SET department_id = '$new_depart_id', course_name = '$new_course_name'
+			WHERE course_id = '$course_id';";
+			$conn->exec($sql);
+		}
+		//All record update
+		else{
+			$sql = "UPDATE OR IGNORE Results
+			SET department_id = '$new_depart_id', course_name = '$new_course_name', description = '$new_result_descrip'
+			WHERE course_id = '$course_id'";
+			$conn->exec($sql);
+		}
+        echo "Updated data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//Delete needs to be careful of children
+function deleteResults($course_id){
+    global $conn;
+    try {
+        // insert data
+        $sql = "DELETE OR IGNORE FROM Cources
+				WHERE course_id = '$course_id';";
+        $conn->exec($sql);
+        echo "Deleted data successfully <br>";
+    }catch (PDOException $e){
+        echo 'Exception : '.$e->getMessage();
+    }
+}
+
+//		***RESULTS END***
+
 function outputData(){
     global $conn;
     try {
