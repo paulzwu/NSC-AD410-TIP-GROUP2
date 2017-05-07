df08748348cd2e2dbba2ac609875a81ad3767d36
Ghebre <geberhane@gmail.com>
2017-05-07 01:37:01 -0700

    php files accessing data from JSON and adding it to database

---
+++ resources/lib/sprint2/empdetails_json.js
@@ -0,0 +1,18 @@
+{
+    "empid": "SJ011MS",
+    "personal": {
+        "name": "Smith Jones",
+        "gender": "Male",
+        "age": "28",
+        "address": {
+            "streetaddress": "7 24th Street",
+            "city": "New York",
+            "state": "NY",
+            "postalcode": "10038"
+        }
+    },
+    "profile": {
+        "designation": "Deputy General",
+        "department": "Finance"
+    }
+}
