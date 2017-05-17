620ab5b08bae62f27e1459f10000e43997877d81
Kari Bullard <kari.e.bullard@gmail.com>
2017-05-16 18:05:52 -0700

    vendor folder

--- Core/Config.php
+++ Core/Config.php
@@ -4,8 +4,8 @@
     //OAuth configurations for canvas authorization
     global $config;
     $config = [
-        'canvasClientId' => '90000000000380',
-        'canvasClientSecret' => 'RpiIUNErVJDJeAYsUYzJsdlQFzI8ihyrHlO5SbJ6r4CFWYZlFEO5NSO5zHmcsKZl',
+        'canvasClientId' => '90000000000383',
+        'canvasClientSecret' => 'onlPUzNNIxEXjisu0Ne3oUcEqPSQzrBpZbGUIS1a1pipl9h9glLZsMSjYk5dmmPd',
         'redirectUri'=> "http://markpfaff.com/projects/NSC-AD410-TIP-GROUP2/admin/",
         // 'redirectUri'=> "http://nsc-ad410-tip-group2.localhost/admin",
         'canvasInstanceUrl'=>'https://northseattle.test.instructure.com'
