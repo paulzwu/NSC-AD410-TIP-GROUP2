3bf4073a0471cd89751c41c7ff45c9ce750965cb
Karalyn Bullard <kbullard@oly.sea>
2017-05-24 14:24:04 -0700

    trying to make db work

--- App/Models/TipEditor.php
+++ App/Models/TipEditor.php
@@ -1,55 +1,4 @@
 <?php
-
-// namespace App\Models;
-
-// use PDO;
-
-// //        This is where you should write your db access functions
-// //        Views will access the data by calling methods from this class using a class instance
-
-// class TipEditor extends \Core\Model
-// {
-
-     /*
-   * Retrieves survey names from DB for populating in a select element
-   */
-    // public static function getSurveys()
-    // {
-    //       $table = 'Surveys';
-    //       $col1 = 'surveyID';
-    //       $col2 = 'surveyName';
-    //       $sqlstmt = "SELECT $col1, $col2 FROM $table;";
-    //       try {
-    //         // $conn = new PDO("sqlite:DB/tip-editor-testDB.sqlite");
-    //         // $db = static::getDB();
-    //         $db = new PDO("sqlite:DB/tip-editor-testDB.sqlite");
-    //         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //         $statement = $db->prepare($sqlstmt);
-    //         $statement->execute();
-    //         $statement->setFetchMode(PDO::FETCH_ASSOC);
-    //         $jsonData = $statement->fetchAll();
-    //         $jsonArray = array('surveyInfo'=>$jsonData);
-    //         // echo json_encode($jsonArray);
-    //         $db = NULL;
-    //       } catch (PDOException $e) {
-    //         echo "PHP Load error: ".$e->getMessage();
-    //       }
-    //     return $jsonArray;
-    // }
-
-
-//     public static function getUser()
-//     {
-//         $user = array (
-//             "name" => "User"
-//         );
-//         return $user;
-//     }
-// }
-
-
-
-
 namespace App\Models;
 
 use PDO;
@@ -73,32 +22,48 @@ class TipEditor extends \Core\Model
         return $user;
     }
 
-         /*
+  /*
    * Retrieves survey names from DB for populating in a select element
    */
     public static function getSurveys()
     {
-       $db->openOrCreateDB();
-          $table = 'Surveys';
-          $col1 = 'surveyID';
-          $col2 = 'surveyName';
-          $sqlstmt = "SELECT $col1, $col2 FROM $table;";
-          try {
-            // $conn = new PDO("sqlite:DB/tip-editor-testDB.sqlite");
-            // $db = static::openOrCreateDB();
-            // $db = new PDO('sqlite:tip-editor-testDB.sqlite');
-            // $db = static::getDB();
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $statement = $db->prepare($sqlstmt);
-            $statement->execute();
-            $statement->setFetchMode(PDO::FETCH_ASSOC);
-            $jsonData = $statement->fetchAll();
-            $jsonArray = array('surveyInfo'=>$jsonData);
-            // echo json_encode($jsonArray);
-            $db = NULL;
-          } catch (PDOException $e) {
-            echo "PHP Load error: ".$e->getMessage();
-          }
-        return $jsonArray;
+       $surveyData = (isset($_POST['saveData'])) ? $_POST['saveData'] : "";
+        $surveyName = (isset($_POST['saveName'])) ? $_POST['saveName'] : "";
+        $jsonData = JSON_decode($surveyData);
+        $table = 'Surveys';
+        $col1 = 'surveyID';
+        $col2 = 'jsonData';
+        $col3 = 'surveyName';
+        $sqlstmt = "INSERT INTO $table($col2, $col3) VALUES ('$surveyData', '$surveyName');";
+        try {
+          $conn = new PDO("sqlite:test.sqlite");
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $statement = $conn->prepare($sqlstmt);
+          $statement->execute();
+          $conn = NULL;
+        } catch (PDOException $e) {
+          echo "PHP Save error: ".$e->getMessage();
+        }
     }
+
+    public static function loadSurveys(){
+        $surveyID = (isset($_POST['ID'])) ? $_POST['ID'] : "";
+        $table = 'Surveys';
+        $col1 = 'surveyID';
+        $col2 = 'jsonData';
+        $sqlstmt = "SELECT $col2 FROM $table WHERE $col1 = '$surveyID';";
+        try {
+          $conn = new PDO("sqlite:test.sqlite");
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $statement = $conn->prepare($sqlstmt);
+          $statement->execute();
+          $surveyData = $statement->fetchColumn();
+          $conn = NULL;
+        } catch (PDOException $e) {
+          echo "PHP Load error: ".$e->getMessage();
+        }
+          print_r($surveyData);
+    }
+
+
 }
