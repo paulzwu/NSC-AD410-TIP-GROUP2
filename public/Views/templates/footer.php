3309dd4a21aa1b6f01378fd23dae42be49ce215c
markpfaff <markdpfaff@gmail.com>
2017-05-30 20:57:54 -0700

    fixes tip assessment styles conflict

--- public/Views/templates/footer.php
+++ public/Views/templates/footer.php
@@ -7,8 +7,8 @@
 
 
     <!--   Core JS Files   -->
-    <script src="assets/js/lib/jquery-3.2.1.min.js" type="text/javascript"></script>
-    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<!--    <script src="assets/js/lib/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>-->
 
     <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
     <script src="assets/js/light-bootstrap-dashboard.js"></script>
@@ -18,6 +18,15 @@
    <script src="assets/js/lib/raphael-2.1.4.min.js"></script>
     <script src="assets/js/lib/justgage.js"></script>
     <script src="assets/js/tipprogress.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.3.0/knockout-min.js"></script>
+	    <script src="https://surveyjs.azureedge.net/0.12.9/survey.ko.min.js"></script>
+	    <script src="https://surveyjs.azureedge.net/0.12.9/surveyeditor.min.js"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.min.js" type="text/javascript"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/worker-json.js" type="text/javascript"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/mode-json.js" type="text/javascript"></script>
+	    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js" type="text/javascript"></script>
     <script>
     			$(document).ready(function() {
 
