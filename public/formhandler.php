c288f2c7769f2f5b3313ec02a5100f10807c35c3
markpfaff <markdpfaff@gmail.com>
2017-06-03 17:46:50 -0700

    adds form support

--- public/formhandler.php
+++ public/formhandler.php
@@ -1,57 +1,33 @@
 <?php
-//This is a very simple PHP script that outputs the name of each bit of information in the browser window, and then sends it all to an email address you add to the script.
-echo "formhandler has run";
-ob_start();
-if (empty($_POST)) {
-	header('Location: '.$_SERVER['HTTP_REFERER']);
-	exit();
+//simple php form
+
+//check for directly accessing page
+if (!isset($_POST['submit'])) {
+   echo "<h1>Error</h1>\n
+      <p>Accessing this page directly is not allowed.</p>";
+   exit;
 }
 
-//Creates function that removes magic escaping, if it's been applied, from values and then removes extra newlines and returns to foil spammers.
-function clear_user_input($value) {
-	if (get_magic_quotes_gpc()) $value=stripslashes($value);
-	$value= str_replace( "\n", '', trim($value));
-	$value= str_replace( "\r", '', $value);
-	return $value;
-	}
+$email = preg_replace("([\r\n])", "", $email);
 
+$find = "/(content-type|bcc:|cc:)/i";
+if (preg_match($find, $name) || preg_match($find, $email) || preg_match($find, $url) || preg_match($find, $comments)) {
+   echo "<h1>Error</h1>\n
+      <p>No meta/header injections, please.</p>";
+   exit;
+}
 
-if ($_POST['comments'] == 'How can we help?') $_POST['comments'] = '';	
+//set title of message
+$message = "Message from TIP Web Application: \n";
 
-//Create body of message by cleaning each field and then appending each name and value to it
-
-$body ="Here is the message from the TIP Assessment Web App:\n";
-
-foreach ($_POST as $key => $value) {
-    if(is_array($value)){ 				// if this post element is a checkbox group or multiple select box
-        $value = implode(', ',$value);	// show array of values selected
-
+//get form data
+foreach ($_POST as $key => $value){
+    if($key == 'submit'){
+        continue;
+    }else{
+        $message .= htmlspecialchars($key).": ".htmlspecialchars($value)."\n";
     }
-
-    $key = clear_user_input($key); 
-    $value = clear_user_input($value);
-    $$key = $value;
-
-    $body .= "$key: $value\n";
 }
 
-
-
-
-$from='From: '. $email . "(" . $name . ")" . "\r\n" . 'Bcc: markdpfaff@gmail.com' . "\r\n";
-// sends bcc to alternate address 
-
-//Creates intelligible subject line that shows where it came from
-$subject = 'Email from the TIP Assessment Web App'; // if your client has more than one web site, you can put the site name here.
-
-// for troubleshooting, uncomment the two lines below. Send your form, and you'll get a browser message showing your results.
-//echo "mail ('clientname@domain.com', $subject, $body, $from);";
-//exit();
-
-//Sends email, with elements created above
-//Replace clientname@domain.com with your client's email address. Put your address here for initial testing, put your client's address for final testing and use.
-mail ('markdpfaff@gmail.com', $subject, $body, $from);
-
-
-header('Location: thanks.php'); // replace "thx.html" with the name and path to your actual thank you page
-
+//send form
+mail('markdpfaff@gmail.com', 'Message from TIP Web Application', $message);
--- public/support.php
+++ public/support.php
@@ -16,7 +16,7 @@ include 'Views/templates/header.php';
                 <div class="row control-group">
                     <div class="form-group col-xs-12 floating-label-form-group controls">
                         <label>Name</label>
-                        <input type="text" class="form-control" placeholder="Name" id="name" required=""
+                        <input type="text" name="name" class="form-control" placeholder="Name" id="name" required=""
                                data-validation-required-message="Please enter your name.">
                         <p class="help-block text-danger"></p>
                     </div>
@@ -24,7 +24,7 @@ include 'Views/templates/header.php';
                 <div class="row control-group">
                     <div class="form-group col-xs-12 floating-label-form-group controls">
                         <label>Email Address</label>
-                        <input type="email" class="form-control" placeholder="Email Address" id="email"
+                        <input type="email" name="email" class="form-control" placeholder="Email Address" id="email"
                                required="" data-validation-required-message="Please enter your email address.">
                         <p class="help-block text-danger"></p>
                     </div>
@@ -32,7 +32,7 @@ include 'Views/templates/header.php';
                 <div class="row control-group">
                     <div class="form-group col-xs-12 floating-label-form-group controls">
                         <label>Phone Number</label>
-                        <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required=""
+                        <input type="tel" name="phone" class="form-control" placeholder="Phone Number" id="phone" required=""
                                data-validation-required-message="Please enter your phone number.">
                         <p class="help-block text-danger"></p>
                     </div>
@@ -40,7 +40,7 @@ include 'Views/templates/header.php';
                 <div class="row control-group">
                     <div class="form-group col-xs-12 floating-label-form-group controls">
                         <label>Message</label>
-                        <textarea rows="5" class="form-control" placeholder="Message" id="message" required=""
+                        <textarea rows="5" name="comments" class="form-control" placeholder="Message" id="message" required=""
                                   data-validation-required-message="Please enter a message."></textarea>
                         <p class="help-block text-danger"></p>
                     </div>
@@ -49,7 +49,7 @@ include 'Views/templates/header.php';
                 <div id="success"></div>
                 <div class="row">
                     <div class="form-group col-xs-12">
-                        <button type="submit" class="btn btn-success btn-lg">Send</button>
+                        <button type="submit" name="submit" class="btn btn-success btn-lg">Send</button>
                     </div>
                 </div>
             </form>
