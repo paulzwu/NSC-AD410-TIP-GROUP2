d2860179e0df7de194b31e281c0fabf1c2402ca8
supreti17 <upretisuraj11@gmail.com>
2017-06-07 17:10:58 -0700

    Updated config file

--- public/config.php
+++ public/config.php
@@ -4,8 +4,8 @@
 
     global $config;
 	$config = [
-        'canvasClientId' => '90000000000383',
-        'canvasClientSecret' => 'onlPUzNNIxEXjisu0Ne3oUcEqPSQzrBpZbGUIS1a1pipl9h9glLZsMSjYk5dmmPd',
+        'canvasClientId' => '90000000000386',
+        'canvasClientSecret' => '2bVdQCmOAqCD1N3HXxqbvk8fYyC0GtqUymqWpleyfl5EQhLfSwirsuc6A8pRjrpd',
         'redirectUri'=> "http://markpfaff.com/projects/NSC-AD410-TIP-GROUP2/public/",
         'canvasInstanceUrl'=>'https://northseattle.test.instructure.com'
     ];
