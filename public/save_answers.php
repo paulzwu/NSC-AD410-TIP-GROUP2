78a07fecb805e7958178bd9e0b7e917115316baf
Joseph Sayler <joseph.sayler@gmail.com>
2017-06-13 21:35:46 -0700

    updated second insert statement in save_answers.php
    changed the insert statement to an update statement. made it really condensed, so it uses multiple subqueries to get the info we need and update the USR_JOIN_ANS_JOIN_SUR table.

--- public/save_answers.php
+++ public/save_answers.php
@@ -3,43 +3,42 @@
  * Saves survey answers to DB
  */
 	include "db_connect.php";
+
+	$oauthID = '110099'; // $_SESSION[OAUTH_ID];
+
 	$quiz_answer = (isset($_POST['Responses'])) ? $_POST['Responses'] : "";
-	$date = date("m d Y H i s");
+	$date = date('Y-m-d h:i:s');
 	$flag = '1';
-	$table = 'ANSWER';
+
+	$table1 = 'ANSWER';
+	$table2 = 'USERS';
+	$table3 = 'SURVEY'
+	$table4 = 'USR_JOIN_ANS_JOIN_SUR';
+	
 	$col1 = 'answerID';
 	$col2 = 'answerJSON';
 	$col3 = 'complete';
 	$col4 = 'time_complete';
-	$sqlstmt = "INSERT INTO $table($col2, $col3, $col4) VALUES ('$quiz_answer', '$flag', '$date');";
-	
-	$oauthID = '123456'; // Place holder for oauthID
-	$sqlstmt2 = "SELECT userID FROM USERS WHERE oauthID = '$oauthID';";
-	$sqlstmt3 = "SELECT $col1 FROM $table WHERE $col4 = '$date';";
-	
+	$col5 = 'userID';
+	$col6 = 'oauthID';
+	$col7 = 'currentTIP';
+	$col8 = 'surveyID';
+
+	$sqlstmt1 = "INSERT INTO $table1($col2, $col3, $col4) VALUES ('$quiz_answer', '$flag', '$date');";
+	$sqlstmt3 = "UPDATE $table4 SET $col1 = (SELECT $col1 FROM $table1 WHERE $col4 = '$date')
+							 WHERE $col5 = (SELECT $col5 FROM $col2 WHERE $col6 = '$oauthID') AND $col8 =
+							(SELECT $col8 FROM $table3 WHERE $col7 = '$flag');"
+
 	try {
-		$statement = $connection->prepare($sqlstmt);
+		$statement = $connection->prepare($sqlstmt1);
 		$statement->execute();
-		
-		$statement = $connection->prepare($sqlstmt2);
-		$statement->execute();
-		$userID = $statement->fetchColumn(0);
-		
+
 		$statement = $connection->prepare($sqlstmt3);
 		$statement->execute();
-		$answerID = $statement->fetchColumn(0);
-	} catch (PDOException $e) {
-		echo "PHP Save error: ".$e->getMessage();
-	}
-	
-	$sqlstmt4 = "INSERT INTO USR_JOIN_ANS_JOIN_SUR(answerID, userID) VALUES('$answerID', '$userID');";
-	
-	try {
-		$statement = $connection->prepare($sqlstmt4);
-		$statement->execute();
+
+		$connection = NULL;
 	} catch (PDOException $e) {
 		echo "PHP Save error: ".$e->getMessage();
 	}
 
-	$connection = NULL;
 ?>
